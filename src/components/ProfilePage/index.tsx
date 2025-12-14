40676fad7fc3d7585733c86e0fd13bda3a7ef85e
pedrovitorino07 <pedrolucasofici@gmail.com>
2025-12-14 14:04:57 -0300

    first commit

---
+++ src/components/ProfilePage/index.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+
+import {
+  Container,
+  Banner,
+  Avatar,
+  ProfileData,
+  LocationIcon,
+  CakeIcon,
+  Followage,
+  EditButton,
+} from './styles';
+
+const ProfilePage: React.FC = () => {
+  return (
+    <Container>
+      <Banner>
+        <Avatar />
+      </Banner>
+
+      <ProfileData>
+        <EditButton outlined>Editar Perfil</EditButton>
+        <h1>Pedro Vitorino</h1>
+        <h2>@_pedrovito</h2>
+
+        <p>
+          Developer at <a href="https://rocketseat.com.br">@Rocketseat</a>
+        </p>
+
+        <ul>
+          <li>
+            <LocationIcon />
+            Pernambuco, Brasil
+          </li>
+          <li>
+            <CakeIcon />
+            Nascido(a) em 01 de setembro de 2004
+          </li>
+        </ul>
+
+        <Followage>
+          <span>
+            seguindo <strong>94</strong>
+          </span>
+          <span>
+            <strong>672 </strong> seguidores
+          </span>
+        </Followage>
+      </ProfileData>
+    </Container>
+  );
+};
+
+export default ProfilePage;
