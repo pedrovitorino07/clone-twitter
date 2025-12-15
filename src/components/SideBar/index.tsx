0201d340e67c05c0806cfd9c8e2530bb48c53560
pedrovitorino07 <pedrolucasofici@gmail.com>
2025-12-15 19:25:09 -0300

    feed and menubar

---
+++ src/components/SideBar/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from './styles';
+
+// import { Container } from './styles';
+
+const SideBar: React.FC = () => {
+  return <Container>
+    
+  </Container>;
+};
+
+export default SideBar;
